1ce12818ca3c30f47453ef16dd79dab73a38f4b0
Helene T <helene.teychene@gmail.com>
2025-10-20 13:59:42 +0200

    1.3 - nivell 2 - exercici 4

--- 1.3_callbacks.js
+++ 1.3_callbacks.js
@@ -2,9 +2,9 @@
 // un nombre i una funció de callback. La funció processar ha d'invocar la funció
 // de callback, passant el nombre com a paràmetre.
 
-const myCallback = (num) => `I have processed ${num}`;
+const myCallback = (num) => console.log(`I have processed ${num}`);
 const processar = (num, callback) => callback(num);
-// --> console.log(processar(4,myCallback))
+processar(4, myCallback)
 
 // 2. Callbacks amb operacions matemàtiques: Escriu una funció calculadora que accepti
 // tres paràmetres: dos nombres i una funció de callback. La funció calculadora
@@ -19,7 +19,22 @@ const calculadora = (num1, num2, add) => add(num1, num2)
 // 2 segons i llavors invocar la funció de callback, passant el nom com a paràmetre.
 
 const saludar = (name) => `Hola ${name}`;
-const esperarISaludar = (name, saludar) => setTimeout(() => {
-    console.log(saludar(name));
+const esperarISaludar = (name, callback) => setTimeout(() => {
+    console.log(callback(name));
 }, 2000);
 
+esperarISaludar("Helene", saludar)
+
+// 4. Callbacks amb arrays: Escriu una funció processarElements que accepti dos
+// paràmetres: un array i una funció de callback. La funció processarElements ha
+// d'invocar la funció de callback per cada element de l'array.
+
+const indexing = (index, item) => {
+    console.log(`${index + 1} : ${item}`);
+};
+
+const processarElements = (arr, indexing) => {
+    for (let i = 0; i < arr.length; i++) {
+        indexing(i, arr[i]);
+    }
+}
