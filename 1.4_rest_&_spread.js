40352be449766dea2642eade31c645a48b804398
Helene T <helene.teychene@gmail.com>
2025-10-20 22:01:41 +0200

    1.4/ nivell 1 - exercici 1

---
+++ 1.4_rest_&_spread.js
@@ -0,0 +1,7 @@
+// 1. Operador Spread en Arrays: Crea dues arrays, array1 i array2. Utilitza
+// l'operador spread per a crear una tercera array que contingui tots els elements
+// de array1 i array2.
+
+const array1 = ["this", "is", "my", 1, "st", "array"]
+const array2 = [6, 9, 11, 23, 60]
+const array3 = [...array1, ...array2]
