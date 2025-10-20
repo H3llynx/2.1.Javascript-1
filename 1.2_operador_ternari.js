69fed6ede1cb5205df77f00186479784647cfeab
Helene T <helene.teychene@gmail.com>
2025-10-20 13:11:32 +0200

    1.2 - nivell 1 - exercic i 2

--- 1.2_operador_ternari.js
+++ 1.2_operador_ternari.js
@@ -4,3 +4,14 @@
 // ha de retornar 'No pots conduir'.
 
 const potConduir = (age) => age >= 18 ? "Pots conduir" : "No pots conduir"
+
+// 2. Ús amb operadors de comparació: Escriu una expressió que utilitzi l'operador
+// ternari per determinar quin dels dos nombres donats (num1 i num2) és més gran.
+// Si num1 és més gran, retorna 'num1 és més gran'. Si no, retorna 'num2 és més
+// gran'.
+
+const greatestNumber = (num1, num2) => {
+    if (num1 > num2) return `${num1} és més gran`;
+    else if (num2 > num1) return `${num2} és més gran`;
+    else return;
+}
