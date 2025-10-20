3dab372acbed12a7e3b8b2009367adf2943efd60
Helene T <helene.teychene@gmail.com>
2025-10-20 21:47:52 +0200

    1.3 / nivell 3 - exercici 5

--- 1.3_callbacks.js
+++ 1.3_callbacks.js
@@ -38,3 +38,19 @@ const processarElements = (arr, indexing) => {
         indexing(i, arr[i]);
     }
 }
+
+// 5. Escriu una funció processarCadena que accepti dos paràmetres: una cadena de
+// caràcters i una funció de callback. La funció processarCadena ha de convertir la
+// cadena a majúscules i llavors invocar la funció de callback amb la cadena
+// transformada.
+
+const print = (str) => {
+    console.log(str);
+}
+const procesarCadena = (str, callback) => {
+    str = str.toUpperCase();
+    callback(str);
+}
+
+let str = "probando la funcion"
+procesarCadena(str, print)
