42daed3e05c9b74f889379b971c366fe5831e974
Helene T <helene.teychene@gmail.com>
2025-10-20 13:03:38 +0200

    nivell2 - exercici 4

--- 1.1_arrow_functions.js
+++ 1.1_arrow_functions.js
@@ -23,3 +23,11 @@ class Person {
         return `Hola, ${this.name}`;
     }
 }
+
+// 4. Funció de fletxa dins d'un loop: Crea una funció anomenada printNumbers que
+// accepti un array de números i utilitzi un loop for per imprimir cada número
+// a la consola utilitzant una funció de fletxa.
+
+const printNumbers = (arr) => {
+    arr.forEach(num => console.log(num))
+}
