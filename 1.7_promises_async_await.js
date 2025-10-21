7ffe5019e599708fb28f1472a69fb10056b9532a
Helene T <helene.teychene@gmail.com>
2025-10-21 12:35:37 +0200

    1.7 / nivell 1 - exercici 2

--- 1.7_promises_async_await.js
+++ 1.7_promises_async_await.js
@@ -4,3 +4,8 @@
 let promesa = new Promise((resolve, _reject) => {
     setTimeout(() => resolve("Hola, món"), 2000);
 });
+
+// 2. Utilització d'una Promesa: Utilitza la promesa creada en l'exercici anterior. Crea
+// un .then que imprimeixi el resultat a la consola.
+
+promesa.then(result => console.log(result));
