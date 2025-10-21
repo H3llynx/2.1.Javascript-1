9f5886e931c248e2ee706e0d8b797b287c170698
Helene T <helene.teychene@gmail.com>
2025-10-21 13:22:29 +0200

    1.7 / nivell 3 - exercici 6

--- 1.7_promises_async_await.js
+++ 1.7_promises_async_await.js
@@ -41,3 +41,19 @@ const printPromise2 = async (promise) => {
     }
     catch (error) { console.error(error) }
 };
+
+// 6. Promise.all: Crea dues promeses que es resolguin després de 2 i 3 segons, respectivament.
+// Utilitza Promise.all per a esperar que ambdues promeses es resolguin, i imprimeix els
+// resultats a la consola.
+
+const promise1 = new Promise((resolve, _reject) => {
+    setTimeout(() => resolve("This is my first promise"), 2000)
+});
+
+const promise2 = new Promise((resolve, _reject) => {
+    setTimeout(() => resolve("This is my second promise"), 3000)
+});
+
+Promise.all([promise1, promise2]).then((result) => {
+    console.log(result);
+});
