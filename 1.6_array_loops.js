1e2d2c2f3049fc738f30e4b3d297d859d577979d
Helene T <helene.teychene@gmail.com>
2025-10-21 12:02:59 +0200

    1.6/ nivell 1 - exercici 2

--- 1.6_array_loops.js
+++ 1.6_array_loops.js
@@ -1,3 +1,10 @@
 // 1. forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola:
 let noms = ['Anna', 'Bernat', 'Clara'];
 noms.forEach(nom => console.log(nom));
+
+// 2. for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la
+// consola.
+
+for (let nom of noms) {
+    console.log(nom);
+}
