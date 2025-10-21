33aac142e3f19e211c42623cee623e9a7e231bde
Helene T <helene.teychene@gmail.com>
2025-10-21 12:57:50 +0200

    1.7/ nivell 1 - exercici 4

--- 1.7_promises_async_await.js
+++ 1.7_promises_async_await.js
@@ -14,10 +14,20 @@ promesa.then(result => console.log(result));
 // igual a 'Hola', i que la rebutgi si l'input és qualsevol altra cosa.
 
 let input = prompt("Escribe algo:");
-let promise = new Promise((resolve, reject) => {
+let promesa2 = new Promise((resolve, reject) => {
     setTimeout(() => {
         if (input === "Hola") resolve("Hola, cómo estás?");
         else reject("Error: se tiene que saludar, primero!")
     }, 2000)
 });
+
+// 4. Ús de async/await: Escriu una funció asíncrona que utilitzi la funció await per a
+// esperar el resultat de la promesa creada a l'exercici 1, i que després imprimeixi aquest
+// resultat a la consola.
+
+const printPromise = async (promise) => {
+    let result = await promise;
+    console.log(result);
+};
+printPromise(promesa);
 
