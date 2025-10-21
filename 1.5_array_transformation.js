3f52279f585870ef57a29293012780f7b667ceda
Helene T <helene.teychene@gmail.com>
2025-10-21 11:03:54 +0200

    1.5/ nivell 2 - exercici 5

--- 1.5_array_transformation.js
+++ 1.5_array_transformation.js
@@ -20,3 +20,12 @@ const firstNumOverTen = array.find(n => n > 10);
 
 const arr = [13, 7, 8, 21];
 const sum = arr.reduce((total, num) => total + num);
+
+// 5. Donat un array "[ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]", crea una funció en una sola 
+// línia que faci el següent:
+//- Filtra els nombres majors o iguals a 10.
+//- Multiplica cada nombre filtrat per 2.
+//- Calcula la suma dels nombres filtrats i multiplicats per 2.
+//- La funció ha de retornar el resultat de la suma.
+
+const filterAndCalculate = (arr) => arr.filter(n => n >= 10).map(n => n * 2).reduce((total, num) => total + num);
