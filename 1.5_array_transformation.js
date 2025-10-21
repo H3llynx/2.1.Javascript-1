27cc80f127f84e3159ed76df02a7b2c641e5bbb1
Helene T <helene.teychene@gmail.com>
2025-10-21 10:43:16 +0200

    1.5 / nivell 1 - execici 1

---
+++ 1.5_array_transformation.js
@@ -0,0 +1,6 @@
+// 1. Map: Teniu un array de números [1, 2, 3, 4]. Crea una nova array que contingui el
+// quadrat de cada número.
+
+const initialArray = [1, 2, 3, 4];
+const newArray = initialArray.map(i => i * i)
+
