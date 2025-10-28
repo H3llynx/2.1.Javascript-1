b141c99171e28afea5504f5360224f1ce1bdea9a
Helene T <helene.teychene@gmail.com>
2025-10-28 11:13:04 +0100

    1.1 / implemented recommendations from reviewer

--- 1.1_arrow_functions.js
+++ 1.1_arrow_functions.js
@@ -4,11 +4,22 @@
 
 const add = (a, b) => a + b
 
+console.log(add(5, 3)); // 8
+console.log(add(10, 20)); // 30
+
+
+
+
 // 2. Funció de fletxa sense paràmetres: Crea una funció de fletxa anomenada
 // randomNumber que no necessiti paràmetres i que retorni un número aleatori
 // entre 0 i 100.
 
 const randomNumber = () => Math.floor(Math.random() * 101);
+
+console.log(randomNumber()); // Shows a random number between 0 and 100
+
+
+
 
 // 3. Ús de 'this' en les funcions de fletxa: Crea una classe person que tingui
 // una propietat name i una funció greet que utilitzi una funció de fletxa. La
@@ -24,6 +35,12 @@ class Person {
     }
 }
 
+let helene = new Person("Helene");
+console.log(helene.greet()) // Hola, Helene
+
+
+
+
 // 4. Funció de fletxa dins d'un loop: Crea una funció anomenada printNumbers que
 // accepti un array de números i utilitzi un loop for per imprimir cada número
 // a la consola utilitzant una funció de fletxa.
@@ -31,6 +48,12 @@ class Person {
 const printNumbers = (arr) => {
     arr.forEach(num => console.log(num))
 }
+
+let array = [4, 3, 2, 1, 0]
+printNumbers(array); // Prints each element of the array in a separate line
+
+
+
 
 // 5. Funció de fletxa amb 'setTimeout': Crea una funció de fletxa que imprimeixi
 // un missatge a la consola després d'esperar 3 segons.
@@ -40,3 +63,5 @@ const delayedMsg = (msg) => {
         console.log(msg);
     }, 3000)
 }
+
+delayedMsg("I am late") // I am late
