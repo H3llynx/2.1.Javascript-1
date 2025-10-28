d59593192c06806971046034967935d9bc56efe2
Helene T <helene.teychene@gmail.com>
2025-10-28 11:25:06 +0100

    1.2 / implemented recommendations from reviewer

--- 1.2_operador_ternari.js
+++ 1.2_operador_ternari.js
@@ -5,22 +5,38 @@
 
 const potConduir = (age) => age >= 18 ? "Pots conduir" : "No pots conduir"
 
+console.log(potConduir(16)) // No pots conduir
+console.log(potConduir(18)) // Pots conduir
+
+
+
+
 // 2. Ús amb operadors de comparació: Escriu una expressió que utilitzi l'operador
 // ternari per determinar quin dels dos nombres donats (num1 i num2) és més gran.
 // Si num1 és més gran, retorna 'num1 és més gran'. Si no, retorna 'num2 és més
 // gran'.
 
 const greatestNumber = (num1, num2) => {
-    if (num1 > num2) return `${num1} és més gran`;
-    else if (num2 > num1) return `${num2} és més gran`;
-    else return;
+    return (num1 > num2) ? `${num1} és més gran` : (num2 > num1) ? `${num2} és més gran` : `${num1} = ${num2}`
 }
+
+console.log(greatestNumber(4, 2)); // 4 és més gran
+console.log(greatestNumber(0, 6)); // 6 es més gran
+console.log(greatestNumber(0, 0)); // 0 = 0
+
+
+
 
 // 3.1 Ús enllaçat d'operadors ternaris: Escriu una expressió que utilitzi enllaços
 // d'operadors ternaris per determinar si un número és positiu, negatiu o zero.
 
-let num;
+let num = -4;
 result = (num > 0) ? "positiu" : (num < 0) ? "negatiu" : "zero";
+
+console.log(result) // negatiu
+
+
+
 
 // 3.2 Operador ternari amb funcions: Crea una funció trobarMaxim que accepti tres
 // paràmetres (a, b, c) i utilitzi l'operador ternari per determinar el valor màxim.
@@ -31,6 +47,11 @@ const trobarMaxim = (a, b, c) => {
     return maxValue
 }
 
+console.log(trobarMaxim(90, -5, 89.99)) // 90
+
+
+
+
 // 4. Operador ternari dins un bucle: Escriu una funció parOImpar que accepti un
 // array de números i utilitzi un bucle per a recórrer l'array. Dins del bucle,
 // utilitza l'operador ternari per a determinar si cada número és parell o imparell.
@@ -40,3 +61,12 @@ const ParOImpar = (arr) => {
         console.log(num % 2 === 0 ? `${num} : parell` : `${num} : imparell`);
     }
 }
+
+const array = [2, 3, 9, 78, 42];
+ParOImpar(array)
+
+// '2 : parell'
+// '3 : imparell'
+// '9 : imparell'
+// '78 : parell'
+// '42 : parell'
