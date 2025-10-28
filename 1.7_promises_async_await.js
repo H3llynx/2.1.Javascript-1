37ed60921439a83fd2d7a2de2ad14c5168947687
Helene T <helene.teychene@gmail.com>
2025-10-28 12:11:29 +0100

    1.7 / implemented reviewer's recommendations

--- 1.7_promises_async_await.js
+++ 1.7_promises_async_await.js
@@ -5,21 +5,30 @@ let promesa = new Promise((resolve, _reject) => {
     setTimeout(() => resolve("Hola, món"), 2000);
 });
 
+
+
+
 // 2. Utilització d'una Promesa: Utilitza la promesa creada en l'exercici anterior. Crea
 // un .then que imprimeixi el resultat a la consola.
 
-promesa.then(result => console.log(result));
+promesa.then(result => console.log(result)); // 'Hola, món'
+
+
+
 
 // 3. Promesa amb reject: Crea una promesa que es resolgui després de 2 segons si l'input és
 // igual a 'Hola', i que la rebutgi si l'input és qualsevol altra cosa.
 
-let input = prompt("Escribe algo:");
+let input = "Hola"; // tested with a real prompt in index.html
 let promesa2 = new Promise((resolve, reject) => {
     setTimeout(() => {
         if (input === "Hola") resolve("Hola, cómo estás?");
         else reject("Error: se tiene que saludar, primero!")
     }, 2000)
 });
+
+
+
 
 // 4. Ús de async/await: Escriu una funció asíncrona que utilitzi la funció await per a
 // esperar el resultat de la promesa creada a l'exercici 1, i que després imprimeixi aquest
@@ -29,7 +38,11 @@ const printPromise = async (promise) => {
     let result = await promise;
     console.log(result);
 };
-printPromise(promesa);
+
+printPromise(promesa); // 'Hola, món'
+
+
+
 
 // 5. Gestió d'errors amb async/await: Modifica la funció de l'exercici 4 per a que capturi
 // qualsevol possible error utilitzant un bloc try/catch.
@@ -42,12 +55,17 @@ const printPromise2 = async (promise) => {
     catch (error) { console.error(error) }
 };
 
+printPromise2(promesa); // 'Hola, món'
+
+
+
+
 // 6. Promise.all: Crea dues promeses que es resolguin després de 2 i 3 segons, respectivament.
 // Utilitza Promise.all per a esperar que ambdues promeses es resolguin, i imprimeix els
 // resultats a la consola.
 
 const promise1 = new Promise((resolve, _reject) => {
-    setTimeout(() => resolve("This is my first promise"), 2000)
+    setTimeout(() => resolve("This is my first promise"), 200)
 });
 
 const promise2 = new Promise((resolve, _reject) => {
@@ -55,5 +73,5 @@ const promise2 = new Promise((resolve, _reject) => {
 });
 
 Promise.all([promise1, promise2]).then((result) => {
-    console.log(result);
+    console.log(result); // [ 'This is my first promise', 'This is my second promise' ]
 });
