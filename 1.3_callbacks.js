a329367c428a4949050a4b46cbf6e92462efb4e9
Helene T <helene.teychene@gmail.com>
2025-10-28 11:31:50 +0100

    1.3 / implemented reviewer's recommendations

--- 1.3_callbacks.js
+++ 1.3_callbacks.js
@@ -4,7 +4,11 @@
 
 const myCallback = (num) => console.log(`I have processed ${num}`);
 const processar = (num, callback) => callback(num);
-processar(4, myCallback)
+
+processar(4, myCallback) // I have processed 4
+
+
+
 
 // 2. Callbacks amb operacions matemàtiques: Escriu una funció calculadora que accepti
 // tres paràmetres: dos nombres i una funció de callback. La funció calculadora
@@ -13,6 +17,11 @@ processar(4, myCallback)
 
 const add = (num1, num2) => num1 + num2
 const calculadora = (num1, num2, add) => add(num1, num2)
+
+console.log(calculadora(11, 5, add)) // 16
+
+
+
 
 // 3. Ús de callbacks en funcions asíncrones: Escriu una funció esperarISaludar
 // que accepti dos paràmetres: un nom i una funció de callback. La funció ha d'esperar
@@ -23,7 +32,10 @@ const esperarISaludar = (name, callback) => setTimeout(() => {
     console.log(callback(name));
 }, 2000);
 
-esperarISaludar("Helene", saludar)
+esperarISaludar("Sasha", saludar) // Hola Sasha
+
+
+
 
 // 4. Callbacks amb arrays: Escriu una funció processarElements que accepti dos
 // paràmetres: un array i una funció de callback. La funció processarElements ha
@@ -33,11 +45,22 @@ const indexing = (index, item) => {
     console.log(`${index + 1} : ${item}`);
 };
 
-const processarElements = (arr, indexing) => {
+const processarElements = (arr, callback) => {
     for (let i = 0; i < arr.length; i++) {
-        indexing(i, arr[i]);
+        callback(i, arr[i]);
     }
 }
+
+let array = ["this", "is", 1, "test", "array"]
+processarElements(array, indexing)
+// '1 : this'
+// '2 : is'
+// '3 : 1'
+// '4 : test'
+// '5 : array'
+
+
+
 
 // 5. Escriu una funció processarCadena que accepti dos paràmetres: una cadena de
 // caràcters i una funció de callback. La funció processarCadena ha de convertir la
@@ -53,4 +76,4 @@ const procesarCadena = (str, callback) => {
 }
 
 let str = "probando la funcion"
-procesarCadena(str, print)
+procesarCadena(str, print) // PROBANDO LA FUNCION
